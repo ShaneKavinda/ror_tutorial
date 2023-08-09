c81eb7c5cc84e4bd3ff1c79a9e8c6532cbfb2492
ShaneKavinda <shanekavinda1999@gmail.com>
2023-08-09 15:50:34 +0530

    first commit

---
+++ config/routes.rb
@@ -0,0 +1,12 @@
+Rails.application.routes.draw do
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  root "articles#index"
+
+  #following code declares that GET/articles requests are mapped into 
+  # "index" action of "ArticlesController"
+
+  resources :articles
+
+end
