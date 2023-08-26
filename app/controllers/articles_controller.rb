d38488c10fb88db8c773cc2e487c3fa761ba42f8
ShaneKavinda <shanekavinda1999@gmail.com>
2023-08-26 15:38:19 +0530

    added user authentication

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,9 @@
 class ArticlesController < ApplicationController
+
+  #add user authentication for every action except index and show
+  http_basic_authenticate_with name:"admin", password: "admin", except: [:index, :show]
+
+
   def index
     @articles = Article.all
   end
